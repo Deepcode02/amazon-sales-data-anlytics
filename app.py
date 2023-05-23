4e59bde8ae12e2480982b640a895d08c1001f8c6
Deepcode02 <Deepcode0211@gmail.com>
2023-05-23 20:00:52 +0530

    image

--- app.py
+++ app.py
@@ -192,5 +192,5 @@ def sentiment():
                            fig2=fig2.to_html())
     
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8000, debug=True)
+  app.run(host='0.0.0.0', port=8000, debug=False)
  
